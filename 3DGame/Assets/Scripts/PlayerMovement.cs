9fac8d075f16c85ebae8b29e2a86fa8b05be504c
Ritik Raj <ritikr379@gmail.com>
2023-02-27 15:58:32 +1300

    .

--- 3DGame/Assets/Scripts/PlayerMovement.cs
+++ 3DGame/Assets/Scripts/PlayerMovement.cs
@@ -4,98 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 6.0f; // player's movement speed
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
 
 
-    public float sensitivity = 100.0f; // mouse sensitivity
-
-    float rotationX;
-    float rotationY;
-
-
-    //public Animator anim;
-
-
-    // player's jump speed
-    public float jumpSpeed = 8.0f;
-    // strength of gravity
-    public float gravity = 20.0f;
-
-    private Vector3 moveDirection = Vector3.zero;
-
-
-    CharacterController charController;
-
+    // Start is called before the first frame update
     void Start()
     {
-        charController = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        Player_Jump();
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
-    }
-
-    void FixedUpdate()
-    {
-        Player_WASD();
-        Player_CursorLook();
-    }
-
-    private void Player_CursorLook()
-    {
-        // Get mouse input on the horizontal axis
-        rotationX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-
-        // Get mouse input on the vertical axis
-        rotationY -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-
-        // Limit the vertical rotation to avoid flipping
-        rotationY = Mathf.Clamp(rotationY, -89, 89);
-
-        // Apply the rotations to the player's camera
-        transform.localRotation = Quaternion.Euler(rotationY, rotationX, 0);
-
-    }
-    private void Player_WASD()
-    {
-        // Get horizontal and vertical inputs from the user
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        // Calculate the direction of movement
-        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
-        moveDirection = transform.TransformDirection(moveDirection);
-        moveDirection *= speed;
-
-        // Apply the movement to the player's character controller
-        charController.Move(moveDirection * Time.deltaTime);
-    }
-
-    private void Player_Jump()
-    {
-        // Check if the player is on the ground
-        if (charController.isGrounded)
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            // Get vertical input from the user-
-            float vertical = Input.GetAxis("Jump");
-
-            // Check if the user pressed the jump button
-            if (vertical > 0)
-            {
-                // Add jump force to the player's move direction
-                moveDirection.y = jumpSpeed;
-            }
+            Jump();
         }
+    }
 
-        // Apply gravity to the player's move direction
-        moveDirection.y -= gravity * Time.deltaTime;
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
 
-        // Apply the movement to the player's character controller
-        charController.Move(moveDirection * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, .1f, ground);
     }
 
 }
